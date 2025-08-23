0eb65c07d52f71cf555f0c32dc34b2e4ac8cf2a2
Wei Sun (Jack) <weisun@google.com>
2025-08-23 00:37:56 -0700

    chore: Ignore hidden files in autoformat.sh
    PiperOrigin-RevId: 798490378

--- autoformat.sh
+++ autoformat.sh
@@ -52,16 +52,16 @@ echo '---------------------------------------'
 echo '|  Auto-formatting src/...'
 echo '---------------------------------------'
 
-find -L src/ -type f -name "*.py" -exec pyink --config pyproject.toml {} +
+find -L src/ -not -path "*/.*" -type f -name "*.py" -exec pyink --config pyproject.toml {} +
 
 echo '---------------------------------------'
 echo '|  Auto-formatting tests/...'
 echo '---------------------------------------'
 
-find -L tests/ -type f -name "*.py" -exec pyink --config pyproject.toml {} +
+find -L tests/ -not -path "*/.*" -type f -name "*.py" -exec pyink --config pyproject.toml {} +
 
 echo '---------------------------------------'
 echo '|  Auto-formatting contributing/...'
 echo '---------------------------------------'
 
-find -L contributing/ -type f -name "*.py" -exec pyink --config pyproject.toml {} +
+find -L contributing/ -not -path "*/.*" -type f -name "*.py" -exec pyink --config pyproject.toml {} +
